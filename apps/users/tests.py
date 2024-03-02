1fde57f24707b54d3aa3eaa7afdaf4b189b0a985
djanmamur <mmsaidov@gmail.com>
2024-03-02 20:40:47 +0500

    Cleanup user tests

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,42 +1,28 @@
-from django.test import TestCase
-from django.core.files.uploadedfile import SimpleUploadedFile
-from .models import User
 from django.contrib.auth.hashers import check_password
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+
+from .models import User
 
 
 class UserModelTest(TestCase):
+    def setUp(self) -> None:
+        photo = SimpleUploadedFile(name="test_image.jpg", content="", content_type="image/jpeg")
+        self.user = User.objects.create(username="testuser", password="password123", mainimg=photo)
 
     def test_user_creation(self):
-        user = User.objects.create(
-            username='testuser',
-            password='password123'
-        )
-        self.assertTrue(isinstance(user, User))
-        self.assertTrue(user.id)
-        self.assertTrue(user.created_at)
-        self.assertTrue(user.updated_at)
-        self.assertNotEqual(user.password, 'password123')
+        self.assertTrue(isinstance(self.user, User))
+        self.assertTrue(self.user.id)
+        self.assertTrue(self.user.created_at)
+        self.assertTrue(self.user.updated_at)
+        self.assertNotEqual(self.user.password, "password123")
 
     def test_password_hashing_on_save(self):
-        password = 'password123'
-        user = User(username='testuser', password=password)
-        user.save()
-        self.assertNotEqual(user.password, password)
-        self.assertTrue(check_password(password, user.password))
+        self.assertNotEqual(self.user.password, "password123")
+        self.assertTrue(check_password("password123", self.user.password))
 
     def test_user_str_method(self):
-        user = User.objects.create(username='testuser', password='password123')
-        self.assertEqual(str(user), user.username)
+        self.assertEqual(str(self.user), self.user.username)
 
     def test_get_main_image_url(self):
-
-        test_image_path = 'apps/users/image.png'  # path to the test image
-        user = User.objects.create(username='testuser', password='password123')
-        user.mainimg = SimpleUploadedFile(
-            name='test_image.png',
-            content=open(test_image_path, 'rb').read(),
-            content_type='image/png'
-        )
-        user.save()
-        self.assertIn('users/test_image.png', user.get_main_image_url())
-
+        self.assertIn("users/test_image", self.user.get_main_image_url())
