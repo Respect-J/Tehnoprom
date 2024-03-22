b07e6c38e7aebe40d9c633e2d56aabe7bda9075f
Respect-J <respect0170@gmail.com>
2024-03-22 14:57:48 +0500

    Added returns for categories and associated brands by collection id

--- apps/categories/urls.py
+++ apps/categories/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CategoryListView, CategoryRetrieveUpdateDelete, CategoriesByCollectionUUID
+from .views import CategoryListView, CategoryRetrieveUpdateDelete, CategoriesByCollectionUUID, CollectionCategoryBrandView
 
 urlpatterns = [
     path("", CategoryListView.as_view(), name="Category-get-create"),
     path('collections/<uuid:collection_id>/', CategoriesByCollectionUUID.as_view(), name='collection-categories'),
     path("<uuid:pk>/", CategoryRetrieveUpdateDelete.as_view(), name="Category-retrieve-update-delete"),
+    path('brands/<uuid:collection_id>/', CollectionCategoryBrandView.as_view(), name='collection-category-brand-detail'),
 ]
