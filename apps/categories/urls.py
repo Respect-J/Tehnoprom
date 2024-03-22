600107aa8c39994872b57e673a2a2585dc9065a8
Respect-J <respect0170@gmail.com>
2024-03-22 20:25:33 +0500

    fixed

--- apps/categories/urls.py
+++ apps/categories/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", CategoryListView.as_view(), name="Category-get-create"),
     path('collections/<uuid:collection_id>/', CategoriesByCollectionUUID.as_view(), name='collection-categories'),
     path("<uuid:pk>/", CategoryRetrieveUpdateDelete.as_view(), name="Category-retrieve-update-delete"),
-    path('brands/<uuid:collection_id>/', CollectionCategoryBrandView.as_view(), name='collection-category-brand-detail'),
+    path('brands/collection/<uuid:collection_id>/', CollectionCategoryBrandView.as_view(), name='collection-category-brand-detail'),
 ]
