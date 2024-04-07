575f1cb5e8c3bba0c7d57c7cc173793a5a62f8a8
Respect-J <respect0170@gmail.com>
2024-04-07 19:51:44 +0500

    update Products model, add payment parameters

--- apps/products/models.py
+++ apps/products/models.py
@@ -11,8 +11,12 @@ class Product(BaseModel):
     title = models.CharField(max_length=256, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     mainimg = models.ImageField(upload_to="img/products/", null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     stock_quantity = models.PositiveIntegerField(null=True, blank=True, default=0)
+    # payment parameters
+    package_code = models.CharField(max_length=256, null=True, blank=True)
+    code = models.CharField(max_length=256, null=True, blank=True)
+    price = models.DecimalField(max_digits=100, decimal_places=2, null=True, blank=True)
+    vat_percent = models.DecimalField(max_digits=100, decimal_places=0, null=True, blank=True)
 
     def __str__(self):
         return self.title
