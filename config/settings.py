ed1b6aba21c92177cabc576737361bfcf556cc2a
Respect-J <respect0170@gmail.com>
2024-05-16 17:34:29 +0500

    fixed cors

--- config/settings.py
+++ config/settings.py
@@ -67,8 +67,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:5173", "http://localhost:3001"]
-
+# CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:5173", "http://localhost:3001"]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
