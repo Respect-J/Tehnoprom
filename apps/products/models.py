6ee594f58841c19b0d86bd68a835151ddbff03ca
Respect-J <respect0170@gmail.com>
2024-08-21 17:10:39 +0500

    add discount_percent for products

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from decimal import Decimal, InvalidOperation, ROUND_HALF_UP
 from apps.brands.models import BrandForCategory, Brands
 from apps.categories.models import Category
 from models import BaseModel
@@ -14,18 +14,41 @@ class Product(BaseModel):
     mainimg = models.ImageField(upload_to="img/products/", null=True, blank=True, verbose_name="Главная картинка")
     stock_quantity = models.PositiveIntegerField(null=True, blank=True, default=0,
                                                  verbose_name="Количество товара (не обязательно для заполнения)")
+    discount_percent = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, default=0,
+                                           verbose_name="Процент скидки")
     # payment parameters
     package_code = models.CharField(max_length=256, null=True, blank=True, verbose_name="Код упаковки товара")
     code = models.CharField(max_length=256, null=True, blank=True, verbose_name="Код товара")
     price = models.DecimalField(max_digits=100, decimal_places=2, null=True, blank=True, verbose_name="Цена товара")
-    priceusd = models.CharField(max_length=100, null=True, blank=True, default=0, verbose_name="Цена в долларах")
-    vat_percent = models.DecimalField(max_digits=100, decimal_places=0, null=True, blank=True, verbose_name="НДС")
+    vat_percent = models.DecimalField(max_digits=2, decimal_places=0, null=True, blank=True, verbose_name="НДС")
 
     def __str__(self):
         return self.title
 
     def get_main_image_url(self):
         return self.mainimg.url if self.mainimg else None
+
+    @property
+    def discounted_price(self):
+        try:
+            if self.price is None or self.discount_percent is None:
+                return self.price
+
+            price = Decimal(self.price)
+            discount_percent = Decimal(self.discount_percent)
+
+            # Проверяем, что discount_percent и price не пустые и корректные
+            if discount_percent == Decimal(0):
+                return 0
+
+            discount = (price * discount_percent) / Decimal(100)
+            final_price = price - discount
+
+            # Округляем цену до двух знаков после запятой
+            return final_price.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+        except (InvalidOperation, TypeError, ValueError):
+            # Если произошла ошибка, вернем исходную цену
+            return self.price
 
     class Meta:
         verbose_name = "Товар"
--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-
 from .models import Product
 
-
 class ProductSerializer(serializers.ModelSerializer):
+    discounted_price = serializers.ReadOnlyField()
+
     class Meta:
         model = Product
         fields = "__all__"
