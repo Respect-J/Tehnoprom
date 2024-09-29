463c4481ea98b00199433d7561a27a29adab5c55
Respect-J <respect0170@gmail.com>
2024-09-29 17:59:14 +0500

    hello world

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,12 +1,39 @@
 from rest_framework import serializers
 from .models import Product, PopularProduct
+from decimal import Decimal, getcontext
+
+def calculate_annuity_payment(P, monthly_rate, months):
+
+    getcontext().prec = 28
+
+    P = Decimal(P)
+    r = Decimal(monthly_rate) / Decimal(100)
+    n = Decimal(months)
+
+    A = P * r * (1 + r) ** n / ((1 + r) ** n - 1)
+
+    return int(A)
+
 
 class ProductSerializer(serializers.ModelSerializer):
     discounted_price = serializers.ReadOnlyField()
+    instalments = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = "__all__"
+
+    def get_instalments(self, obj):
+
+        price = int(obj.price)
+        monthly_rate = 5
+        months = 12
+
+        if price and price > 0:
+            return calculate_annuity_payment(price, monthly_rate, months)
+        return None
+
+
 
 
 class PopularProductSerializer(serializers.ModelSerializer):
