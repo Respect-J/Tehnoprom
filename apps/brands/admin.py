fad54fdc7ff87daca71e6fcec89c3a171903fe0c
Respect-J <respect0170@gmail.com>
2024-11-05 14:47:37 +0500

    added filter by admin

--- apps/brands/admin.py
+++ apps/brands/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import BrandForCategory, Brands
 
-admin.site.register(BrandForCategory)
+
 admin.site.register(Brands)
+
+
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'category_id')
+    search_fields = ('title', )
+    list_filter = ('category_id', 'created_at')
+
+
+admin.site.register(BrandForCategory, RequestAdmin)
